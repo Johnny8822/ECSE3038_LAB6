f54f377e376666675ccda18933a3f5b3e6b91ffc
Johnny8822 <146615330+Johnny8822@users.noreply.github.com>
2024-03-17 23:53:19 -0500

    More stuff

--- include/env.h
+++ include/env.h
@@ -1,6 +1,7 @@
-const char* ssid ="MonaConnect"; 
+const char* ssid ="Digicel_WiFi_3FcH"; 
 const char* password = "XZ55dDMn";
-String endpoint ="https://https://ecse3038-temp-light-lab.onrender.com/message"; 
+String endpoint ="https://https://ecse3038-temp-light-lab.onrender.com/"; 
 const char* api_key = "Joyce#1615"
-
+#define LED_PIN 6
+#define ONE_WIRE_BUS 4 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,74 @@
 #include <Arduino.h> 
-#include <LiquidCrystal.h> 
 #include <ArduinoJson.h>  
 #include <OneWire.h>
 #include <HTTPClient.h> 
 #include "env.h"
 #include <DallasTemperature.h> 
 
-#define LED_PIN 6
-#define ONE_WIRE_BUS 4 
-Onewire oneWire(ONE_WIRE_BUS); 
 
-DallasTemperature sensors(&oneWire)
+OneWire oneWire(ONE_WIRE_BUS); 
 
-LiquidCrystal My_LCD(22,23,5,18,19,21);
+
+DallasTemperature sensors(&oneWire);
+
+//LiquidCrystal My_LCD(22,23,5,18,19,21);
 
 unsigned long lastTime = 0; 
 unsigned long Timerdelay =2000;
 // put function declarations here:
 
+void getLight(void){
+  
+  HTTPClient http;  
+  String serverPath_light = endpoint + "light";
+  http.begin(serverPath_light); 
+  http.addHeader("Content-Type", "application/json"); 
+  String RequestBody;  
+  
+  int httpResponseCode = http.GET();
+    if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    String responseBody = http.getString(); 
+    Serial.println(responseBody); 
+    JsonDocument doc;
 
-void patchTemp(char * body){
-  HTTPClient http 
-  http.begin(endpoint); 
-  http.addHeader("Content-Type", "application/json") 
+  DeserializationError error = deserializeJson(doc, responseBody);
 
-  http.PUT(body); 
+  if (error) {
+  Serial.print("deserializeJson() failed: ");
+  Serial.println(error.c_str());
+  return;
+   }
+
+   bool light = doc["light"];
+   
+  if(light == LOW){
+  digitalWrite(LED_PIN, light);  
+  }
+   else {
+  digitalWrite(LED_PIN, light); 
+    }
+  } 
+  http.end();
+} 
+
+void patchTemp(float temperature){
+  HTTPClient http;  
+  String serverPath_temp = endpoint + "temp";
+  http.begin(serverPath_temp); 
+  http.addHeader("Content-Type", "application/json"); 
+  String RequestBody;  
+
+  JsonDocument doc;  
+  doc ["temp"] = 32; 
+  doc.shrinkToFit(); 
+  serializeJson(doc, RequestBody);
+
+  int httpRequestCode = http.PATCH(RequestBody);
+  http.PUT(RequestBody); 
   Serial.print("HERE IS THE RESPONSE");
-  Serial.println(body);
+  Serial.println(temperature);
   Serial.println(http.getString());  
   Serial.println();
 
@@ -55,41 +97,12 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:  
-  
-
-  HTTPClient http; 
-  
-  http.begin(endpoint.c_str());  
-
-  http.GET();  
-  http.addHeader("api-key", api_key);
-  int httpResponseCode = http.GET(); 
-  
   delay(1000);  
-  sensors.requestTemperatures(); 
-  
+  sensors.requestTemperatures();    
+  float x = sensors.getTempCByIndex(); 
+  patchTemp(x);
+  Serial.println(x);  
 
-  if (httpResponseCode>0) {
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-    String responseBody = http.getString(); 
-    Serial.println(responseBody); 
-
-    JsonDocument doc; 
-    DeserializationError error = deserializeJson(doc, responseBody);
-
-if (error){
-    Serial.print("deserializeJson()failed: ");
-    Serial.println(error.c_str());
-    return;
-   }
-   String line_1 = doc["line_1"];  
-   String line_2 = doc["line_2"]; 
-   
-
-
-  
-
-  }
 }
+
 // put function definitions here:
